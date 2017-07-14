1e6a6526b70910bfe57dc76ef58863434de828da
Boris Babic <boris.ivan.babic@gmail.com>
2017-07-14 17:29:29 +0200

    Prioritize bin commands in locate helper

--- pre_commit_hooks/helpers/locate.sh
+++ pre_commit_hooks/helpers/locate.sh
@@ -12,12 +12,12 @@ prefixed_local_command="php $local_command"
 
 if [ -f "$vendor_command" ]; then
     exec_command=$vendor_command
+elif [ -f "$bin_command" ]; then
+    exec_command=$bin_command
 elif hash $global_command 2>/dev/null; then
     exec_command=$global_command
 elif [ -f "$local_command" ]; then
     exec_command=$prefixed_local_command
-elif [ -f "$bin_command" ]; then
-    exec_command=$bin_command
 else
     echo -e "${bldred}No valid ${title} found!${txtrst}"
     echo "Please have one available as one of the following:"
