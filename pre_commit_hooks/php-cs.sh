c67d36ee8f3a6d58c2b84bd86c6965e5c131a7bf
Boris Babic <boris.ivan.babic@gmail.com>
2017-07-14 12:19:04 +0200

    Improve phpcs command locating

--- pre_commit_hooks/php-cs.sh
+++ pre_commit_hooks/php-cs.sh
@@ -20,30 +20,17 @@ msg_color_none='\033[0m' # No Color
 
 # Loop through the list of paths to run php codesniffer against
 echo -en "${msg_color_yellow}Begin PHP Codesniffer ...${msg_color_none} \n"
-phpcs_local_exec="phpcs.phar"
-phpcs_command="php $phpcs_local_exec"
 
-# Check vendor/bin/phpunit
-phpcs_vendor_command="vendor/bin/phpcs"
-phpcs_global_command="phpcs"
-if [ -f "$phpcs_vendor_command" ]; then
-	phpcs_command=$phpcs_vendor_command
-else
-    if hash phpcs 2>/dev/null; then
-        phpcs_command=$phpcs_global_command
-    else
-        if [ -f "$phpcs_local_exec" ]; then
-            phpcs_command=$phpcs_command
-        else
-            echo "No valid PHP Codesniffer executable found! Please have one available as either $phpcs_vendor_command, $phpcs_global_command or $phpcs_local_exec"
-            exit 1
-        fi
-    fi
-fi
+# Possible command names of this tool
+local_command="phpcs.phar"
+vendor_command="vendor/bin/phpcs"
+global_command="phpcs"
+bin_command="bin/phpcs"
+source $DIR/helpers/locate.sh
 
 phpcs_files_to_check="${@:2}"
 phpcs_args=$1
-phpcs_command="$phpcs_command $phpcs_args $phpcs_files_to_check"
+phpcs_command="$exec_command $phpcs_args $phpcs_files_to_check"
 
 echo "Running command $phpcs_command"
 command_result=`eval $phpcs_command`
